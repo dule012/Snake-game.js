7d73a60d46d823e5eb1fcb9415605f60bd389748
unknown <dusanmihajlovic012@gmail.com>
2018-08-05 02:06:27 +0200

    finished

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ var top1 = 0,
     wrapper = document.querySelector('.wrapper'),
     congratulations = document.querySelector('p'),
     newGame = document.querySelector('.newGame'),
+    mode = document.querySelector('.mode'),
+    easy = document.querySelector('.easy'),
+    medium = document.querySelector('.medium'),
+    hard = document.querySelector('.hard'),
+    pOfMode = document.querySelectorAll('.mode p'),
+    body = document.querySelector('body'),
     arrOfWidth = [],
     arrOfHeight = [],
     splicedWidth = [],
@@ -25,42 +31,64 @@ var top1 = 0,
     div,
     snakeLength = 0,
     intervaTwoClicks,
-    counterBetweenTwoClicks = 41
-for (i = 0, j = 0; i < 59; i++) {
+    counterBetweenTwoClicks = 0,
+    value = 0
+for (i = 0, j = 0; i < 60; i++) {
     if (i % 2 == 0) {
-        arrOfWidth[j] = i * 10
-        j++
+        arrOfWidth[l] = i * 10
+        l++
     }
 }
-for (i = 0, j = 0; i < 29; i++) {
+for (i = 0, j = 0; i < 30; i++) {
     if (i % 2 == 0) {
-        arrOfHeight[j] = i * 10
-        j++
+        arrOfHeight[k] = i * 10
+        k++
     }
 }
-food.style.left = arrOfWidth[Math.floor(Math.random() * 30)] + 'px'
-food.style.top = arrOfHeight[Math.floor(Math.random() * 15)] + 'px'
+food.style.left = arrOfWidth[Math.floor(Math.random() * l)] + 'px'
+food.style.top = arrOfHeight[Math.floor(Math.random() * k)] + 'px'
 snake.style.top = top1 + 'px'
 snake.style.left = left + 'px'
+score.style.display = 'none'
+body.style.backgroundColor = 'white'
+
+for (i = 0; i < pOfMode.length; i++) {
+    (function (i) {
+        pOfMode[i].addEventListener('click', () => {
+            if (pOfMode[i].textContent == 'Easy') {
+                counterBetweenTwoClicks = 60
+                value = 60
+                mode.style.display = 'none'
+                score.style.display = 'block'
+                body.style.backgroundColor = 'rgba(195, 222, 231, 0.89)'
+            } else if (pOfMode[i].textContent == 'Medium') {
+                counterBetweenTwoClicks = 40
+                value = 40
+                mode.style.display = 'none'
+                score.style.display = 'block'
+                body.style.backgroundColor = 'rgba(195, 222, 231, 0.89)'
+            } else {
+                counterBetweenTwoClicks = 20
+                value = 20
+                mode.style.display = 'none'
+                score.style.display = 'block'
+                body.style.backgroundColor = 'rgba(195, 222, 231, 0.89)'            }
+        })
+    })(i)
+}
 
 function Game(e) {
 
-    if (e.keyCode == 37 && rightPressed == false && counterBetweenTwoClicks == 41) { // LEFT arrow uslov ako se krece desno da ne moze levo nego samo gore i dole
+    if (e.keyCode == 37 && rightPressed == false && counterBetweenTwoClicks == value) { // LEFT arrow uslov ako se krece desno da ne moze levo nego samo gore i dole
         if (leftPressed == false) { // uslov da kada se drzi pritisnuto u jednom trenutku ukopa se u mestu snake jer stalnim clearInterval i davanjem intervala se ukopa 
             clearInterval(interval)
             counterBetweenTwoClicks = 0
             setTimeout(() => {
-                counterBetweenTwoClicks = 41
-            }, 41)
+                counterBetweenTwoClicks = value
+            }, value)
             interval = setInterval(() => {
                 snakeBodyArr.unshift({ left: parseInt(snake.style.left), top: parseInt(snake.style.top) })
-                for (i = 0; i < snakeBodyArr.length; i++) {
-                    for (j = 0; j < arrOfWidth.length; j++) {
-                        if (snakeBodyArr[i].left == arrOfWidth[j]) {
 
-                        }
-                    }
-                }
                 if (parseInt(snake.style.left) <= 0) {
                     snake.style.left = table.offsetWidth - 20 + 'px'
                     left = table.offsetWidth - 20 // da bih se odmah pojavila u polju ne kockicu ispred polja
@@ -73,8 +101,6 @@ function Game(e) {
                     }
                 }
                 if (parseInt(snake.style.left) == parseInt(food.style.left) && parseInt(snake.style.top) == parseInt(food.style.top)) {
-                    food.style.left = arrOfWidth[Math.floor(Math.random() * 30)] + 'px'
-                    food.style.top = arrOfHeight[Math.floor(Math.random() * 15)] + 'px'
                     div = document.createElement('div')
                     div.setAttribute('class', 'snake')
                     snake.after(div)
@@ -83,6 +109,37 @@ function Game(e) {
                     snakeLength++
                     snakeBodyArr.length = snakeLength + 1
                     score.textContent = 'Your score is: ' + snakeLength
+                    for (i = 0; i < snakeBody.length; i++) {
+                        for (j = 0; j < arrOfWidth.length; j++) {
+                            if (parseInt(snakeBody[i].style.left) == arrOfWidth[j]) {
+                                splicedWidth.push(arrOfWidth[j])
+                                arrOfWidth.splice(j, 1)
+                                j--
+                            }
+                        }
+                        for (j = 0; j < arrOfHeight.length; j++) {
+                            if (parseInt(snakeBody[i].style.top) == arrOfHeight[j]) {
+                                splicedHeight.push(arrOfHeight[j])
+                                arrOfHeight.splice(j, 1)
+                                j--
+                            }
+                        }
+                    }
+
+                    food.style.left = arrOfWidth[Math.floor(Math.random() * (arrOfWidth.length - 1))] + 'px'
+                    food.style.top = arrOfHeight[Math.floor(Math.random() * (arrOfHeight.length - 1))] + 'px'
+
+                    for (i = 0; i < splicedWidth.length; i++) {
+                        arrOfWidth.push(splicedWidth[i])
+                        splicedWidth.shift()
+                        i--
+                    }
+                    for (i = 0; i < splicedHeight.length; i++) {
+                        arrOfHeight.push(splicedHeight[i])
+                        splicedHeight.shift()
+                        i--
+                    }
+
                 }
                 for (i = 1; i < snakeBody.length; i++) {
                     if (snake.style.left == snakeBody[i].style.left && snake.style.top == snakeBody[i].style.top) {
@@ -95,20 +152,20 @@ function Game(e) {
                         window.removeEventListener('keydown', Game)
                     }
                 }
-            }, 40)
+            }, value)
         }
         leftPressed = true
         topPressed = false
         bottomPressed = false
         rightPressed = false
-    } else if (e.keyCode == 38 && bottomPressed == false && counterBetweenTwoClicks == 41) { // TOP arrow
+    } else if (e.keyCode == 38 && bottomPressed == false && counterBetweenTwoClicks == value) { // TOP arrow
 
         if (topPressed == false) {
             clearInterval(interval)
             counterBetweenTwoClicks = 0
             setTimeout(() => {
-                counterBetweenTwoClicks = 41
-            }, 41)
+                counterBetweenTwoClicks = value
+            }, value)
             interval = setInterval(() => {
                 snakeBodyArr.unshift({ left: parseInt(snake.style.left), top: parseInt(snake.style.top) })
                 if (parseInt(snake.style.top) <= 0) {
@@ -123,8 +180,6 @@ function Game(e) {
                     }
                 }
                 if (parseInt(snake.style.left) == parseInt(food.style.left) && parseInt(snake.style.top) == parseInt(food.style.top)) {
-                    food.style.left = arrOfWidth[Math.floor(Math.random() * 30)] + 'px'
-                    food.style.top = arrOfHeight[Math.floor(Math.random() * 15)] + 'px'
                     div = document.createElement('div')
                     div.setAttribute('class', 'snake')
                     snake.after(div)
@@ -133,6 +188,37 @@ function Game(e) {
                     snakeLength++
                     snakeBodyArr.length = snakeLength + 1
                     score.textContent = 'Your score is: ' + snakeLength
+                    for (i = 0; i < snakeBody.length; i++) {
+                        for (j = 0; j < arrOfWidth.length; j++) {
+                            if (parseInt(snakeBody[i].style.left) == arrOfWidth[j]) {
+                                splicedWidth.push(arrOfWidth[j])
+                                arrOfWidth.splice(j, 1)
+                                j--
+                            }
+                        }
+                        for (j = 0; j < arrOfHeight.length; j++) {
+                            if (parseInt(snakeBody[i].style.top) == arrOfHeight[j]) {
+                                splicedHeight.push(arrOfHeight[j])
+                                arrOfHeight.splice(j, 1)
+                                j--
+                            }
+                        }
+                    }
+
+                    food.style.left = arrOfWidth[Math.floor(Math.random() * (arrOfWidth.length - 1))] + 'px'
+                    food.style.top = arrOfHeight[Math.floor(Math.random() * (arrOfHeight.length - 1))] + 'px'
+
+                    for (i = 0; i < splicedWidth.length; i++) {
+                        arrOfWidth.push(splicedWidth[i])
+                        splicedWidth.shift()
+                        i--
+                    }
+                    for (i = 0; i < splicedHeight.length; i++) {
+                        arrOfHeight.push(splicedHeight[i])
+                        splicedHeight.shift()
+                        i--
+                    }
+
                 }
                 for (i = 1; i < snakeBody.length; i++) {
                     if (snake.style.left == snakeBody[i].style.left && snake.style.top == snakeBody[i].style.top) {
@@ -145,19 +231,19 @@ function Game(e) {
                         window.removeEventListener('keydown', Game)
                     }
                 }
-            }, 40)
+            }, value)
         }
         leftPressed = false
         topPressed = true
         bottomPressed = false
         rightPressed = false
-    } else if (e.keyCode == 39 && leftPressed == false && counterBetweenTwoClicks == 41) { // RIGHT arrow
+    } else if (e.keyCode == 39 && leftPressed == false && counterBetweenTwoClicks == value) { // RIGHT arrow
         if (rightPressed == false) {
             clearInterval(interval)
             counterBetweenTwoClicks = 0
             setTimeout(() => {
-                counterBetweenTwoClicks = 41
-            }, 41)
+                counterBetweenTwoClicks = value
+            }, value)
             interval = setInterval(() => {
                 snakeBodyArr.unshift({ left: parseInt(snake.style.left), top: parseInt(snake.style.top) })
                 if (parseInt(snake.style.left) + 20 >= table.offsetWidth) {
@@ -172,8 +258,6 @@ function Game(e) {
                     }
                 }
                 if (parseInt(snake.style.left) == parseInt(food.style.left) && parseInt(snake.style.top) == parseInt(food.style.top)) {
-                    food.style.left = arrOfWidth[Math.floor(Math.random() * 30)] + 'px'
-                    food.style.top = arrOfHeight[Math.floor(Math.random() * 15)] + 'px'
                     div = document.createElement('div')
                     div.setAttribute('class', 'snake')
                     snake.after(div)
@@ -182,6 +266,37 @@ function Game(e) {
                     snakeLength++
                     snakeBodyArr.length = snakeLength + 1
                     score.textContent = 'Your score is: ' + snakeLength
+                    for (i = 0; i < snakeBody.length; i++) {
+                        for (j = 0; j < arrOfWidth.length; j++) {
+                            if (parseInt(snakeBody[i].style.left) == arrOfWidth[j]) {
+                                splicedWidth.push(arrOfWidth[j])
+                                arrOfWidth.splice(j, 1)
+                                j--
+                            }
+                        }
+                        for (j = 0; j < arrOfHeight.length; j++) {
+                            if (parseInt(snakeBody[i].style.top) == arrOfHeight[j]) {
+                                splicedHeight.push(arrOfHeight[j])
+                                arrOfHeight.splice(j, 1)
+                                j--
+                            }
+                        }
+                    }
+
+                    food.style.left = arrOfWidth[Math.floor(Math.random() * (arrOfWidth.length - 1))] + 'px'
+                    food.style.top = arrOfHeight[Math.floor(Math.random() * (arrOfHeight.length - 1))] + 'px'
+
+                    for (i = 0; i < splicedWidth.length; i++) {
+                        arrOfWidth.push(splicedWidth[i])
+                        splicedWidth.shift()
+                        i--
+                    }
+                    for (i = 0; i < splicedHeight.length; i++) {
+                        arrOfHeight.push(splicedHeight[i])
+                        splicedHeight.shift()
+                        i--
+                    }
+
                 }
                 for (i = 1; i < snakeBody.length; i++) {
                     if (snake.style.left == snakeBody[i].style.left && snake.style.top == snakeBody[i].style.top) {
@@ -194,20 +309,20 @@ function Game(e) {
                         window.removeEventListener('keydown', Game)
                     }
                 }
-            }, 40)
+            }, value)
         }
         leftPressed = false
         topPressed = false
         bottomPressed = false
         rightPressed = true
 
-    } else if (e.keyCode == 40 && topPressed == false && counterBetweenTwoClicks == 41) { // BOTTOM arrow
+    } else if (e.keyCode == 40 && topPressed == false && counterBetweenTwoClicks == value) { // BOTTOM arrow
         if (bottomPressed == false) {
             clearInterval(interval)
             counterBetweenTwoClicks = 0
             setTimeout(() => {
-                counterBetweenTwoClicks = 41
-            }, 41)
+                counterBetweenTwoClicks = value
+            }, value)
             interval = setInterval(() => {
                 snakeBodyArr.unshift({ left: parseInt(snake.style.left), top: parseInt(snake.style.top) })
                 if (parseInt(snake.style.top) + 20 >= table.offsetHeight) {
@@ -222,8 +337,6 @@ function Game(e) {
                     }
                 }
                 if (parseInt(snake.style.left) == parseInt(food.style.left) && parseInt(snake.style.top) == parseInt(food.style.top)) {
-                    food.style.left = arrOfWidth[Math.floor(Math.random() * 30)] + 'px'
-                    food.style.top = arrOfHeight[Math.floor(Math.random() * 15)] + 'px'
                     div = document.createElement('div')
                     div.setAttribute('class', 'snake')
                     snake.after(div)
@@ -232,6 +345,37 @@ function Game(e) {
                     snakeLength++
                     snakeBodyArr.length = snakeLength + 1
                     score.textContent = 'Your score is: ' + snakeLength
+                    for (i = 0; i < snakeBody.length; i++) {
+                        for (j = 0; j < arrOfWidth.length; j++) {
+                            if (parseInt(snakeBody[i].style.left) == arrOfWidth[j]) {
+                                splicedWidth.push(arrOfWidth[j])
+                                arrOfWidth.splice(j, 1)
+                                j--
+                            }
+                        }
+                        for (j = 0; j < arrOfHeight.length; j++) {
+                            if (parseInt(snakeBody[i].style.top) == arrOfHeight[j]) {
+                                splicedHeight.push(arrOfHeight[j])
+                                arrOfHeight.splice(j, 1)
+                                j--
+                            }
+                        }
+                    }
+
+                    food.style.left = arrOfWidth[Math.floor(Math.random() * (arrOfWidth.length - 1))] + 'px'
+                    food.style.top = arrOfHeight[Math.floor(Math.random() * (arrOfHeight.length - 1))] + 'px'
+
+                    for (i = 0; i < splicedWidth.length; i++) {
+                        arrOfWidth.push(splicedWidth[i])
+                        splicedWidth.shift()
+                        i--
+                    }
+                    for (i = 0; i < splicedHeight.length; i++) {
+                        arrOfHeight.push(splicedHeight[i])
+                        splicedHeight.shift()
+                        i--
+                    }
+
                 }
                 for (i = 1; i < snakeBody.length; i++) {
                     if (snake.style.left == snakeBody[i].style.left && snake.style.top == snakeBody[i].style.top) {
@@ -244,7 +388,7 @@ function Game(e) {
                         window.removeEventListener('keydown', Game)
                     }
                 }
-            }, 40)
+            }, value)
         }
         leftPressed = false
         topPressed = false
@@ -256,7 +400,7 @@ function Game(e) {
 
 function repeatGame() {
     // setTimeout(() => { 
-    //     snakeBody = document.getElementsByClassName('snake')  // NE BRISE  SVE OBJEKTE NEGO POLOVINU!!! NIKO MI NIJE JASNO
+    //     snakeBody = document.getElementsByClassName('snake')  // NE BRISE  SVE OBJEKTE NEGO POLOVINU!!! NIKO MI NIJE JASNO (Resenje je u splice-u)
     //     console.log(snakeBody.length)
     //     for (i = 1; i < snakeBody.length; i++) {
     //         table.removeChild(snakeBody[i])
@@ -278,7 +422,7 @@ function repeatGame() {
     //         div,
     //         snakeLength = 0,
     //         intervaTwoClicks,
-    //         counterBetweenTwoClicks = 41
+    //         counterBetweenTwoClicks = value
     //     wrapper.style.display = 'none'
     //     wrapper.style.animation = ''
     //     snake.style.backgroundColor = 'yellow'
@@ -290,9 +434,9 @@ function repeatGame() {
 
 window.addEventListener('keydown', Game)
 
-setInterval(() => { // brise na 10s niz napunjen pozicijama
-    snakeBodyArr.splice(snakeLength + 1, snakeBodyArr.length - snakeLength)
-    setTimeout(() => {
-        console.log(snakeBodyArr)
-    }, 1)
-}, 10000)
+// setInterval(() => { // brise na 10s niz napunjen pozicijama
+//     snakeBodyArr.splice(snakeLength + 1, snakeBodyArr.length - snakeLength)
+//     setTimeout(() => {
+//         console.log(snakeBodyArr)
+//     }, 1)
+// }, 10000)
